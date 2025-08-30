b6abf33d0f9bccf7e6a1763ec8a9d54dcbe6a275
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 23:08:06 +0600

    solve 3rd problem of 'user input 'in C++

--- Problem04.cpp
+++ Problem04.cpp
@@ -1,5 +1,20 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [User-Input]
-//               Problem : 👉👉 [4][3] Area of Rectangle
+//               Problem : 👉👉 [4] Area of Rectangle
 // ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<< "Enter length " ;
+    cin>> x;
+    int y ;
+    cout<<"Enter width " ;
+    cin>> y;
+    cout<<"Area of Rectangle = "<< x*y<<endl;
+
+
+
+    return 0;
+}
