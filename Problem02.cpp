872334bb96107fdca0646acd39ad33bb20b61d70
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 22:50:20 +0600

    solve 2nd problem of 'user input 'in C++

---
+++ Problem02.cpp
@@ -0,0 +1,15 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [User-Input]
+//               Problem : 👉👉 [1] Sum of Two Numbers
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+  int x, y ;
+  cout<<"Enter 2 Number : ";
+  cin>> x>> y;
+  cout<<"Sum is " <<x+y;
+  cout<<endl;
+    return 0;
+}
