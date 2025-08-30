fabf2d2f106c4a64928fe345af4aff30aaba748a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 22:44:01 +0600

    solve 1st problem of 'user input 'in C++

---
+++ Problem01.cpp
@@ -0,0 +1,16 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [User-Input]
+//               Problem : 👉👉 [1] Area of a Circle
+// ================================================
+
+#include <iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<< "Enter Radius Value ";
+    cin>> r;
+    cout <<"Solution : " << r*r*3.1416<<endl;
+
+    return 0;
+}
