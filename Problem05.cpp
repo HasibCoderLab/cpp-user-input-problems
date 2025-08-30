f207015494e6b6c89ef25e2442f0d6c4e6182934
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 23:16:02 +0600

    solve 3rd problem of 'user i nput 'in C++

--- Problem05.cpp
+++ Problem05.cpp
@@ -3,3 +3,13 @@
 //               Topic   : [User-Input]
 //               Problem : 👉👉 [5] Perimeter of a Square
 // ================================================
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x ;
+    cout<< "Enter value  :";
+    cin>> x;
+    cout<< "Perimeter of a Square " << 4*x<<endl;
+    return 0;
+}
