a68e89d78ca89a444a57ec352f64040688fe0539
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 23:26:02 +0600

    solve 6th  problem of 'user input 'in C++

--- Problem06.cpp
+++ Problem06.cpp
@@ -3,3 +3,21 @@
 //               Topic   : [User-Input]
 //               Problem : 👉👉 [6] Simple Interest Calculation
 // ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p;
+    cout << "Enter Principal : ";
+    cin >> p;
+
+    float t;
+    cout << "Enter Time  ";
+    cin >> t;
+
+    float r;
+    cout << "Enter Rate ";
+    cin >> r;
+    cout<<"Simple Interest = " << (p*t*r) /100<<endl;
+    return 0;
+}
