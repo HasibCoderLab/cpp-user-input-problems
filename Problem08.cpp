7fec713ac22d739fdbb3181eb580a1878de7497a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-31 07:01:37 +0600

    solve 4th problem of 'user input 'in C++

--- Problem08.cpp
+++ Problem08.cpp
@@ -2,5 +2,26 @@
 //               Language: C++ / Cpp
 //               Topic   : [User-Input]
 //               Problem : 👉👉 [8] Calculate Remainder & Quotient
-//     👉 Dividend আর Divisor ইনপুট নিয়ে quotient আর remainder 
+//     👉 Dividend আর Divisor ইনপুট নিয়ে quotient আর remainder
 // ================================================
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int Dividend;
+    cout << "Enter Dividend : ";
+    cin >> Dividend;
+
+    int Divisor;
+    cout << "Enter Divisor : ";
+    cin >> Divisor;
+
+    int  Quotient = Dividend / Divisor;
+    int Remainder = Dividend  % Divisor; 
+    cout<<"Quotient = " <<Quotient <<endl;
+    cout<<"Remainder = " << Remainder <<endl;
+    
+    return 0;
+}
