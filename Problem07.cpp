4d62bafaa0b1d531ef30d1feaaf0f965e9ec0a34
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-31 06:34:23 +0600

    solve 7th problem of 'user input 'in C++

--- Problem07.cpp
+++ Problem07.cpp
@@ -1,6 +1,16 @@
-
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [User-Input]
 //               Problem : 👉👉 [7] Temperature Conversion
+//               👉 Celsius ইনপুট নিয়ে Fahrenheit-এ রূপান্তর 
 // ================================================
+
+#include <iostream>
+using namespace std;
+int main(){
+    float value;
+    cout<<"Enter Celsius : ";
+    cin>> value;
+    cout<<"Fahrenheit Temperature is " <<(value * 9/5) + 32 << "°" <<endl;
+    return 0;
+}
